ac84046635106ea09cc6d7aa6a15f4dd64b38a2e
Lemonish <patrikmihelson@gmail.com>
2024-09-11 15:43:08 +0300

    spring boot project initialization

---
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/TicketguruAppApplication.java
@@ -0,0 +1,13 @@
+package com.eliteinnovators.ticketguru.ticketguru_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketguruAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketguruAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliteinnovators/ticketguru/ticketguru_app/TicketguruAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliteinnovators.ticketguru.ticketguru_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketguruAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
