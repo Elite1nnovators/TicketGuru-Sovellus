f91b0ae2b03290040f1451a3ad9b8c2eff9a96f2
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-09-21 15:40:06 +0300

    Lisätty OrderDetailsRepository

---
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/repository/OrderDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.eliteinnovators.ticketguru.ticketguru_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eliteinnovators.ticketguru.ticketguru_app.domain.OrderDetails;
+
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long>{
+
+}
