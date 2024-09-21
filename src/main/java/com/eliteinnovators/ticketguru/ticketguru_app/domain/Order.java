e73ed5a76bfae468c1621a03e099a30edb69c045
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-09-21 15:29:09 +0300

    korjattu ManyToOne ja OneToMany yhteyksiä ja lisätty yhteys OrderDetails -tauluun

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Order.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Order.java
@@ -1,12 +1,14 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.domain;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -16,28 +18,28 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long order_id;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "salesPerson_id")
     private Salesperson salesperson;
 
     private Date orderDate;
 
-    // Lisää yhteys orderDetails tauluun
-    //@ManyToOne(mappedBy = "order")
-    //private List<OrderDetail> orderDetails;
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetails> orderDetails;
 
 
     public Order () {
 
     }
 
-    public Order(Customer customer, Date orderDate, Long order_id, Salesperson salesperson) {
+    public Order(Customer customer, Date orderDate, List<OrderDetails> orderDetails, Long order_id, Salesperson salesperson) {
         this.customer = customer;
         this.orderDate = orderDate;
+        this.orderDetails = orderDetails;
         this.order_id = order_id;
         this.salesperson = salesperson;
     }
@@ -74,10 +76,19 @@ public class Order {
         this.orderDate = orderDate;
     }
 
+    public List<OrderDetails> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetails> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
     @Override
     public String toString() {
         return "Order [order_id=" + order_id + ", customer=" + customer + ", salesperson=" + salesperson
-                + ", orderDate=" + orderDate + "]";
+                + ", orderDate=" + orderDate + ", orderDetails=" + orderDetails + "]";
     }
+ 
 
 }
