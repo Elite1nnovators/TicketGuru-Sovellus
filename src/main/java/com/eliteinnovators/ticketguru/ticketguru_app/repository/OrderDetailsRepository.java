82cd345d11fb3a7e98b18b0af229e938a8580be0
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-09-23 18:40:34 +0300

    lisätty OrderDetailsRepositoryyn ' findByOrderDetailId '

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/repository/OrderDetailsRepository.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/repository/OrderDetailsRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eliteinnovators.ticketguru.ticketguru_app.domain.OrderDetails;
 
+
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long>{
+
+    OrderDetails findByOrderDetailId(Long orderDetailId);
 
 }
