c04786a8931c7cb480e1d2899a119181ee47c094
Ida Kabris <bhi273@myy.haaga-helia.fi>
2024-09-24 15:26:16 +0300

    Lisätty puuttuvat importit Customer ja Salesperson luokkiin

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Customer.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Customer.java
@@ -1,10 +1,17 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.domain;
 
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Customer {
--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Salesperson.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Salesperson.java
@@ -1,10 +1,16 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.domain;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 
 @Entity
