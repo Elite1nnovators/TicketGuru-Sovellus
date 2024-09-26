40d4d0c742f9756e6a654508b7d6a48792a6014d
Lemonish <patrikmihelson@gmail.com>
2024-09-26 12:29:58 +0300

    otettu kommentit pois ticket-yhteyden muodostukseen

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Event.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Event.java
@@ -1,11 +1,13 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.domain;
 
-//import java.util.List;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToMany;
 import java.util.Date;
 
 @Entity
@@ -21,9 +23,17 @@ public class Event {
     private String eventDescription;
 
     //Tähän lisätään yhteys Ticket-tauluun:
-    //@OneToMany(cascade = CascadeType.ALL, mappedBy = "Event")
-    //private List<Ticket> tickets;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "event")
+    private List<Ticket> tickets;
 
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 
     public Event() {
     }
