49f292eb23ed9681918feb8318ba48f3bc7b41b9
Lemonish <patrikmihelson@gmail.com>
2024-09-30 18:57:04 +0300

    lisätty getmapping event/{id}:lle

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/TicketGuruController.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/TicketGuruController.java
@@ -1,6 +1,9 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +29,16 @@ public class TicketGuruController {
     @GetMapping("/events")
     public Iterable<Event> getAllEvents() {
         return eRepo.findAll();
+    }
+
+    @GetMapping("/event/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable("id") Long eventId) {
+        Optional<Event> event = eRepo.findById(eventId);
+        if(event.isPresent()) {
+            return ResponseEntity.ok(event.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/event")
