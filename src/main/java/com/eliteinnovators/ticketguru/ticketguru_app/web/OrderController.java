142954e83ba10ad05f14a68eae507b97e1784688
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-10-05 13:52:41 +0300

    muokattu kommentteja

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/OrderController.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/OrderController.java
@@ -1,12 +1,26 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.eliteinnovators.ticketguru.ticketguru_app.domain.Order;
+import com.eliteinnovators.ticketguru.ticketguru_app.repository.OrderRepository;
 
 @RestController
 public class OrderController {
 
+     @Autowired
+    OrderRepository orderRepo;
+
+
     // MYYNTITAPAHTUMIEN REST -ENDPOINTIT
-    /*@GetMapping("/orders")
+    @GetMapping("/orders")
     public Iterable<Order> getAllOrders() {
         return orderRepo.findAll();
     }
@@ -16,7 +30,7 @@ public class OrderController {
         return orderRepo.findById(orderId).orElse(null);
     }
 
-     kesken
+   
     @PostMapping("/orders")
     public Order newOrder(@RequestBody Order newOrder) {
         return orderRepo.save(newOrder);
@@ -24,7 +38,7 @@ public class OrderController {
 
     //kesken
     @PutMapping("orders/{orderId}")
-    Order editOrder(@RequestBody Order editedOrder, @PathVariable Long OrderId) {
+    Order editOrder(@RequestBody Order editedOrder, @PathVariable Long orderId) {
         editedOrder.setOrderId(orderId);
         return orderRepo.save(editedOrder);
     }
@@ -34,5 +48,5 @@ public class OrderController {
     public Iterable<Order> deleteOrder(@PathVariable("orderId") Long orderId) {
         orderRepo.deleteById(orderId);
         return orderRepo.findAll();
-    } */
+    }
 }
