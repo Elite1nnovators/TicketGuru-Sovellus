f1fd9bcd622b17d6f00f50d466e73dc0c28a5852
Lemonish <patrikmihelson@gmail.com>
2024-10-08 14:05:05 +0300

    Päivitetty toimimaan OrderMapperin avulla

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/service/OrderService.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/service/OrderService.java
@@ -1,44 +1,69 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.service;
 
 import com.eliteinnovators.ticketguru.ticketguru_app.domain.*;
+import com.eliteinnovators.ticketguru.ticketguru_app.exception.CustomerNotFoundException;
+import com.eliteinnovators.ticketguru.ticketguru_app.exception.SalespersonNotFoundException;
+import com.eliteinnovators.ticketguru.ticketguru_app.mapper.OrderMapper;
+import com.eliteinnovators.ticketguru.ticketguru_app.repository.CustomerRepository;
+import com.eliteinnovators.ticketguru.ticketguru_app.repository.SalespersonRepository;
 import com.eliteinnovators.ticketguru.ticketguru_app.web.*;
-import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private SalespersonRepository SalespersonRepository;
+
     public OrderDTO convertToOrderDTO(Order order) {
-        Customer customer = order.getCustomer();
-        Salesperson salesperson = order.getSalesperson();
-        
-        CustomerDTO customerDTO = new CustomerDTO(
-            customer.getCustomerId(),
-            customer.getFirstName(),
-            customer.getLastName(),
-            customer.getEmail(),
-            customer.getPhone(),
-            customer.getCity()
-        );
-        
-        SalespersonDTO salespersonDTO = new SalespersonDTO(
-            salesperson.getSalespersonId(),
-            salesperson.getFirstName(),
-            salesperson.getLastName(),
-            salesperson.isAdmin()
-        );
-        
+        OrderDTO orderDTO = orderMapper.toOrderDTO(order);
+        if(order.getCustomer() != null) {
+            orderDTO.setCustomerFirstName(order.getCustomer().getFirstName());
+            orderDTO.setCustomerLastName(order.getCustomer().getLastName());
+        }
+        if (order.getSalesperson() != null) {
+            orderDTO.setSalespersonFirstName(order.getSalesperson().getFirstName());
+            orderDTO.setSalespersonLastName(order.getSalesperson().getLastName());
+        }
+
+        // OrderDetailsDTO:n täyttö
         List<OrderDetailsDTO> orderDetailsDTOs = new ArrayList<>();
         for (OrderDetails orderDetails : order.getOrderDetails()) {
-            OrderDetailsDTO orderDetailsDTO = new OrderDetailsDTO(
-                orderDetails.getTicket().getId(), 
-                orderDetails.getQuantity(),
-                orderDetails.getUnitPrice()
-            );
+            OrderDetailsDTO orderDetailsDTO = new OrderDetailsDTO();
+            if (orderDetails.getTicket() != null) {
+                orderDetailsDTO.setTicketId(orderDetails.getTicket().getId());
+            }
+            orderDetailsDTO.setQuantity(orderDetails.getQuantity());
+            orderDetailsDTO.setUnitPrice(orderDetails.getUnitPrice());
             orderDetailsDTOs.add(orderDetailsDTO);
         }
 
-        return new OrderDTO(orderDetailsDTOs, order.getOrderId(), customer.getCustomerId(), salesperson.getSalespersonId(), order.getOrderDate(), customerDTO, salespersonDTO);
+        orderDTO.setOrderDetails(orderDetailsDTOs);
+        return orderDTO;
+    }
+
+    public Order convertToOrder(OrderDTO orderDTO) {
+        Order order = orderMapper.toOrder(orderDTO);
+
+        Customer customer = customerRepository.findById(orderDTO.getCustomerId())
+                .orElseThrow(() -> new CustomerNotFoundException("Customer not found"));
+        order.setCustomer(customer);
+
+        Salesperson salesperson = SalespersonRepository.findById(orderDTO.getSalespersonId())
+                .orElseThrow(() -> new SalespersonNotFoundException("Salesperson not found"));
+        order.setSalesperson(salesperson);
+
+        return order;
     }
 }
--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/OrderDTO.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/OrderDTO.java
@@ -1,48 +1,32 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.web;
 
 import java.util.List;
-
 import java.util.Date;
 
 
 public class OrderDTO {
-    private List<OrderDetailsDTO> orderDetails;
     private Long orderId;
     private Long customerId;
+    private String customerFirstName;
+    private String customerLastName;
     private Long salespersonId;
+    private String salespersonFirstName;
+    private String salespersonLastName;
     private Date orderDate;
-    private CustomerDTO customer;
-    private SalespersonDTO salesperson;
-
-    public OrderDTO() {}
-
-    public OrderDTO(List<OrderDetailsDTO> orderDetails, Long orderId, Long customerId, Long salespersonId,
-            Date orderDate, CustomerDTO customer, SalespersonDTO salesperson) {
-        this.orderDetails = orderDetails;
+    private List<OrderDetailsDTO> orderDetails;
+    public OrderDTO() {
+    }
+    public OrderDTO(Long orderId, Long customerId, String customerFirstName, String customerLastName,
+            Long salespersonId, String salespersonFirstName, String salespersonLastName, Date orderDate,
+            List<OrderDetailsDTO> orderDetails) {
         this.orderId = orderId;
         this.customerId = customerId;
+        this.customerFirstName = customerFirstName;
+        this.customerLastName = customerLastName;
         this.salespersonId = salespersonId;
+        this.salespersonFirstName = salespersonFirstName;
+        this.salespersonLastName = salespersonLastName;
         this.orderDate = orderDate;
-        this.customer = customer;
-        this.salesperson = salesperson;
-    }
-    public Long getCustomerId() {
-        return customerId;
-    }
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-    public Long getSalespersonId() {
-        return salespersonId;
-    }
-    public void setSalespersonId(Long salespersonId) {
-        this.salespersonId = salespersonId;
-    }
-
-    public List<OrderDetailsDTO> getOrderDetails() {
-        return orderDetails;
-    }
-    public void setOrderDetails(List<OrderDetailsDTO> orderDetails) {
         this.orderDetails = orderDetails;
     }
     public Long getOrderId() {
@@ -51,22 +35,52 @@ public class OrderDTO {
     public void setOrderId(Long orderId) {
         this.orderId = orderId;
     }
+    public Long getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+    public Long getSalespersonId() {
+        return salespersonId;
+    }
+    public void setSalespersonId(Long salespersonId) {
+        this.salespersonId = salespersonId;
+    }
+    public String getSalespersonFirstName() {
+        return salespersonFirstName;
+    }
+    public void setSalespersonFirstName(String salespersonFirstName) {
+        this.salespersonFirstName = salespersonFirstName;
+    }
+    public String getSalespersonLastName() {
+        return salespersonLastName;
+    }
+    public void setSalespersonLastName(String salespersonLastName) {
+        this.salespersonLastName = salespersonLastName;
+    }
     public Date getOrderDate() {
         return orderDate;
     }
     public void setOrderDate(Date orderDate) {
         this.orderDate = orderDate;
     }
-    public CustomerDTO getCustomer() {
-        return customer;
+    public List<OrderDetailsDTO> getOrderDetails() {
+        return orderDetails;
     }
-    public void setCustomer(CustomerDTO customer) {
-        this.customer = customer;
-    }
-    public SalespersonDTO getSalesperson() {
-        return salesperson;
-    }
-    public void setSalesperson(SalespersonDTO salesperson) {
-        this.salesperson = salesperson;
+    public void setOrderDetails(List<OrderDetailsDTO> orderDetails) {
+        this.orderDetails = orderDetails;
     }
 }
