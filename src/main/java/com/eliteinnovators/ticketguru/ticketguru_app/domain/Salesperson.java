33a825999cee0c7a228f6b42d7264ded39f906ca
Henri Kulmala <bhi259@myy.haaga-helia.fi>
2024-10-17 10:00:49 +0300

    Salesperson validointi

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Salesperson.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Salesperson.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,27 +14,44 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Salesperson {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long salespersonId;
 
+    @NotBlank(message = "Username must not be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     @Column(name = "password", nullable = false)
     private String passwordHash;
 
     private boolean isAdmin;
 
-    private String firstName, lastName, phone;
+    @NotBlank(message = "First name must not be blank")
+    @Size(min = 1, max = 50, message = "First name must be between 1 and 50 characters")
+    private String firstName;
 
-    @OneToMany (cascade = CascadeType.ALL, mappedBy = "salesperson")
+    @NotBlank(message = "Last name must not be blank")
+    @Size(min = 1, max = 50, message = "Last name must be between 1 and 50 characters")
+    private String lastName;
+
+    @Pattern(regexp = "\\d{10}", message = "Phone number must be 10 digits")
+    private String phone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "salesperson")
     @JsonManagedReference(value = "salesperson-order")
     private List<Order> orders = new ArrayList<>();
+
 
     public Salesperson() {}
     
