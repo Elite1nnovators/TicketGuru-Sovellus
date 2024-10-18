31d3ec983f614497617df42bb4e4ade27ef42218
Lemonish <patrikmihelson@gmail.com>
2024-10-18 17:57:36 +0300

    Lisätty Ticketille relaatio Orderiin ja poistettu relaatio OrderDetailsiin

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Ticket.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/domain/Ticket.java
@@ -1,20 +1,6 @@
 package com.eliteinnovators.ticketguru.ticketguru_app.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-
+import jakarta.persistence.*;
 
 @Entity
 public class Ticket {
@@ -25,75 +11,54 @@ public class Ticket {
     private String ticketCode;
     private boolean isValid;
 
-    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
-    @JsonManagedReference(value = "ticket-orderDetails")
-    private List<OrderDetails> orderDetails = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 
     @ManyToOne
     @JoinColumn(name = "eventTicketType_id")
-    @JsonBackReference(value = "eventTicketType-ticket")
     private EventTicketType eventTicketType;
-
     
-    public Ticket() {
-    }
-
+    public Ticket() {}
     public Ticket(EventTicketType eventTicketType, String ticketCode, boolean isValid) {
         this.eventTicketType = eventTicketType;
         this.ticketCode = ticketCode;
         this.isValid = isValid;
-
     }
-
-    public List<OrderDetails> getOrderDetails() {
-        return orderDetails;
+    public Ticket(EventTicketType eventTicketType, Order order, String ticketCode, boolean isValid) {
+        this.eventTicketType = eventTicketType;
+        this.order = order;
+        this.ticketCode = ticketCode;
+        this.isValid = isValid;
     }
-
-    public void setOrderDetails(List<OrderDetails> orderDetails) {
-        this.orderDetails = orderDetails;
-        for (OrderDetails orderDetail : orderDetails) {
-            orderDetail.setTicket(this);
-        }
+    public Order getOrder() {
+        return order;
     }
-
+    public void setOrder(Order order) {
+        this.order = order;
+    }
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getTicketCode() {
         return ticketCode;
     }
-
     public void setTicketCode(String ticketCode) {
         this.ticketCode = ticketCode;
     }
-
     public boolean isValid() {
         return isValid;
     }
-
     public void setValid(boolean isValid) {
         this.isValid = isValid;
     }
-
     public EventTicketType getEventTicketType() {
         return eventTicketType;
     }
-
     public void setEventTicketType(EventTicketType eventTicketType) {
         this.eventTicketType = eventTicketType;
     }
-
-    @Override
-    public String toString() {
-        return "Ticket [id=" + id + ", ticketCode=" + ticketCode + ", isValid=" + isValid + ", orderDetails="
-                + orderDetails + ", eventTicketType=" + eventTicketType + "]";
-    }
-
-    
-
 }
