bd27ac71788689d53ed409444e131978438ae7cc
Lemonish <patrikmihelson@gmail.com>
2024-10-18 17:59:22 +0300

    Korjattu OrderMapper toimimaan uusilla relaatioilla

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/mapper/OrderMapper.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/mapper/OrderMapper.java
@@ -19,36 +19,52 @@ public interface OrderMapper {
     @Mapping(source = "salesperson.firstName", target = "salespersonFirstName")
     @Mapping(source = "salesperson.lastName", target = "salespersonLastName")
     OrderDTO toOrderDTO(Order order);
+    /*
+     * qualifiedByName => apumetodi oikeaan mappaykseen, ks. alempana "default Salesperson idToSalesperson"-metodi esimerkiksi
+     */
+    @Mapping(target = "customer", source = "customerId", qualifiedByName = "idToCustomer")
+    @Mapping(target = "salesperson", source = "salespersonId", qualifiedByName = "idToSalesperson")
+    Order toOrder(OrderDTO orderDTO, @Context CustomerRepository customerRepository, @Context SalespersonRepository salespersonRepository, @Context EventTicketTypeRepository eventTicketTypeRepository);
 
-    @Mapping(target = "customer", source = "customerId")
-    @Mapping(target = "salesperson", source = "salespersonId")
-    Order toOrder(OrderDTO orderDTO, @Context CustomerRepository customerRepository, @Context SalespersonRepository salespersonRepository);
 
-    @Mapping(source = "ticket.id", target = "ticketId")
+    @Mapping(source = "eventTicketType.id", target = "eventTicketTypeId")
     OrderDetailsDTO toOrderDetailsDTO(OrderDetails orderDetails);
 
-    @Mapping(target = "ticket.id", source = "ticketId")
-    OrderDetails toOrderDetails(OrderDetailsDTO orderDetailsDTO, @Context TicketRepository ticketRepository);
+    @Mapping(target = "eventTicketType", source = "eventTicketTypeId", qualifiedByName = "idToEventTicketType")
+    OrderDetails toOrderDetails(OrderDetailsDTO orderDetailsDTO, @Context EventTicketTypeRepository eventTicketTypeRepository);
 
     List<OrderDTO> toOrderDTOs(List<Order> orders);
     List<OrderDetailsDTO> toOrderDetailsDTOs(List<OrderDetails> orderDetails);
-    List<OrderDetails> toOrderDetailsList(List<OrderDetailsDTO> orderDetailsDTO, @Context TicketRepository ticketRepository);
+    List<OrderDetails> toOrderDetailsList(List<OrderDetailsDTO> orderDetailsDTO, @Context EventTicketTypeRepository eventTicketTypeRepository);
 
-    // TODO mapCustomer CustomerMapperiin kun valmis
-    default Customer mapCustomer(Long customerId, @Context CustomerRepository customerRepository) {
+    /*
+     * Metodit mappaamaan Customer, Salesperson tai EventTicketType olioksi Id:n perusteella
+     * toOrder ja toOrderDetails tarvitsee näitä, jotta Id:t mappaantyy oikein olioiksi
+     */
+    @Named("idToCustomer")
+    default Customer idToCustomer(Long customerId, @Context CustomerRepository customerRepository) {
+        if(customerId == null) {
+            return null;
+        }
         return customerRepository.findById(customerId)
             .orElseThrow(() -> new CustomerNotFoundException("Customer with ID " + customerId + " not found"));
     }
 
-    // TODO mapSalesperson SalespersonMapperiin kun valmis
-    default Salesperson mapSalesperson(Long salespersonId, @Context SalespersonRepository salespersonRepository) {
+    @Named("idToSalesperson")
+    default Salesperson idToSalesperson(Long salespersonId, @Context SalespersonRepository salespersonRepository) {
+        if(salespersonId == null) {
+            return null;
+        }
         return salespersonRepository.findById(salespersonId)
             .orElseThrow(() -> new SalespersonNotFoundException("Salesperson with ID " + salespersonId + " not found"));
     }
 
-    // TODO mapTicket TicketMapperiin kun valmis
-    default Ticket mapTicket(Long ticketId, @Context TicketRepository ticketRepository) {
-        return ticketRepository.findById(ticketId)
-            .orElseThrow(() -> new RuntimeException("Ticket with ID " + ticketId + " not found")); // TODO RTE -> ticketin omaksi erroriksi kun löytyy
+    @Named("idToEventTicketType")
+    default EventTicketType idToEventTicketType(Long eventTicketTypeId, @Context EventTicketTypeRepository eventTicketTypeRepository) {
+        if(eventTicketTypeId == null) {
+            return null;
+        }
+        return eventTicketTypeRepository.findById(eventTicketTypeId)
+            .orElseThrow(() -> new EventTicketTypeNotFoundException("EventTicketType with ID " + eventTicketTypeId + " not found"));
     }
 }
