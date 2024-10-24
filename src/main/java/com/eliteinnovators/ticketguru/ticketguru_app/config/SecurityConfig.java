fd8b4cb58d842d2ad4bd8955a6ec264bc57753cc
otvamaria <maria.otva@myy.haaga-helia.fi>
2024-10-24 12:43:55 +0300

    lisätty SecurityConfig luokkaan userDetailsService ja PasswordEncoder (vielä kommentoituina siellä)

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/config/SecurityConfig.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/config/SecurityConfig.java
@@ -3,15 +3,20 @@ package com.eliteinnovators.ticketguru.ticketguru_app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 // Kommentoituna sillä jos ottaa toimintaan ennen UserDetailsServiceä -> Ei ole oikeuksia suorittaa API-pyyntöjä :)
 
-/* 
+/*
 @Configuration
 public class SecurityConfig {
-
+ 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -24,16 +29,42 @@ public class SecurityConfig {
         
         return http.build();
     }
+--> "Admin & Salesperson" käyttäjätiedot (UserDetailsService)?
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsManager = new InMemoryUserDetailsManager();
+
+        userDetailsManager.createUser(
+            User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build()
+        );
+
+        userDetailsManager.createUser(
+            User.withUsername("salesperson")
+                .password(passwordEncoder().encode("salesperson"))
+                .roles("SALESPERSON")
+                .build()
+        );
+
+        return userDetailsManager;
+    }
+
+
+//--> Mitkä ovat järkevät käyttäjänimet/roolit ja mitä endpointteja niillä voi käsitellä?
+
+
+
+//--> PasswordEncoder + Täytyy soveltaa luokkiin, jossa on salasanat esim. (Customer) => PasswordEncoder.encode(PasswordHash) tms.
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
-
-
-
-    --> "Admin & Salesperson" käyttäjätiedot (UserDetailsService)?
-    
-        Mitkä ovat järkevät käyttäjänimet/roolit ja mitä endpointteja niillä voi käsitellä?
-
-
-    --> PasswordEncoder + Täytyy soveltaa luokkiin, jossa on salasanat esim. (Customer) => PasswordEncoder.encode(PasswordHash) tms.
 
 */
