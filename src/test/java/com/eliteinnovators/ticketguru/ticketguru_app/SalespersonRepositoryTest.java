7ec46ad3666804b6a9a83d1fb93a819d03b5959a
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-18 15:41:39 +0200

    poistettu turhia importteja

--- src/test/java/com/eliteinnovators/ticketguru/ticketguru_app/SalespersonRepositoryTest.java
+++ src/test/java/com/eliteinnovators/ticketguru/ticketguru_app/SalespersonRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.eliteinnovators.ticketguru.ticketguru_app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
