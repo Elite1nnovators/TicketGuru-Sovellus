84396ae83153bc238e3771cdc5649ed71078e065
rowinamarie <148778198+rowinamarie@users.noreply.github.com>
2024-11-22 11:03:44 +0200

    poistettu käyttämättömät importit

--- src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/AuthController.java
+++ src/main/java/com/eliteinnovators/ticketguru/ticketguru_app/web/AuthController.java
@@ -5,10 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
-
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
